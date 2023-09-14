0883ee7fe16d0ff25d3e5e8ac3fb075d823cd253
Bryan Vislao Chavez <bvislao95@gmail.com>
2023-09-14 14:34:53 -0500

    update file grafbaseconfig

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-
+// @ts-ignore
 const SolicitudesIngreso = g.model('Solicitudes',{
   dni:g.string().length({min:2,max:100}),
   nombre:g.string(),
@@ -22,27 +22,28 @@ const SolicitudesIngreso = g.model('Solicitudes',{
   fechaRegistro:g.datetime().default(new Date())
 });
 
-
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({min:2,max:100}),
   email:g.string().unique(),
   rol:g.string(),
   avatarUrl: g.url(),
   description:g.string().optional(),
-})
+});
 
-
+//@ts-ignore
 const Viajes = g.model('Viajes',{
-  title:g.string(),
+  title:g.string().search(),
   maxInscripciones:g.int(),
   inscripcionesActuales:g.int(),
-  participantes: g.relation(Participantes).optional().list().optional()
-})
+  participantes: g.relation(()=> Participantes).optional().list().optional()
+});
 
+// @ts-ignore
 const Participantes = g.model('Participantes',{
   title:g.string(),
   Viajes:g.relation(Viajes).optional()
-})
+});
 
 
 
